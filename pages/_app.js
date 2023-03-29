c68cdc3898e203b87258b8c6a37c5327e419a168
Vishal Thakur <vishal.thakur@ethinos.com>
2023-03-29 18:30:19 +0530

    Landing Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider>
+			<Navbar />
+			<Component {...pageProps} />
+			<Footer />
+		</ChakraProvider>
+	);
 }
